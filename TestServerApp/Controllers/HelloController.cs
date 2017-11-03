18dd16cda1ef00ac69448ca44900c6b82c01813f
robinabdullah <robinabdullah>
2017-11-03 14:44:09 +0600

    Add project files.

---
+++ TestServerApp/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestServerApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Customer_Sale_List()
+        {
+            return View(new POSNew12Entities().Customer_Sale.ToList());
+        }
+    }
+}
---
+++ TestServerApp/Store Data.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Net.NetworkInformation;
+using System.Security.AccessControl;
+using System.Security.Principal;
+namespace TestServerApp.Controllers
+{
+    
+    class DB
+    {
+        public static POSNew12Entities db = new POSNew12Entities();
+        public static void resetConnString()
+        {
+            db.Dispose();
+            db = new POSNew12Entities();
+        }
+        public static void DBSubmitChanges()
+        {
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.StackTrace);
+            }
+        }
+    }
+   
+    class ProductTableData : DB
+    {
+        public static IQueryable<Product> getAllProducts()
+        {
+            try
+            {
+                var te = from e in db.Products
+                         orderby e.Unique_Barcode.StartsWith("Y")
+                         select e;
+
+                return te;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+      
+        public static IQueryable<string> getAllProductTypes()
+        {
+            try
+            {
+                //var txts = File.ReadAllLines(ProductTypePath).ToList();
+                var txts = DB.db.Products
+                           .Select(c => new { c.Type, c.Unique_Barcode })
+                           .Distinct()
+                           .OrderByDescending(x => x.Unique_Barcode)
+                           .Select(x => x.Type);
+
+
+                return txts;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+       
+      
+    }
+    
+}
